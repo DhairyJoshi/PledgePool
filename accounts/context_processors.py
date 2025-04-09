0885fa904faf0770d97982ab44bf9f05ab7eea74
Dhairy Joshi <joshidhairy1994@gmail.com>
2025-04-09 16:38:19 +0530

    header_fix

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,6 +1,18 @@
+from .models import pledgepool_user  # Adjust this import based on your app structure
+
 def user_context(request):
+    username = request.session.get('username', '')
+    user_obj = None
+
+    if username:
+        try:
+            user_obj = pledgepool_user.objects.get(username=username)
+        except pledgepool_user.DoesNotExist:
+            user_obj = None 
+
     return {
-        'user_name': request.session.get('name', ''),
+        'user_name': username,
         'user_role': request.session.get('role', ''),
         'is_logged_in': request.session.get('user_id') is not None,
+        'user_obj': user_obj
     }
